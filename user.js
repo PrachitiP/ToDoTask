1ef81e9c9ad9cc0d9427dd4ec1bc6221f73f3664
Patil <prapa@softwareag.com>
2021-03-24 17:58:45 +0530

    Database Schema

---
+++ user.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+     firstname:{
+         type:String,
+         required:true
+     },
+     lastname:{
+        type:String,
+        required:true
+     },
+     email:{
+        type:String,
+        required:true,
+        unique:true
+     },
+     phone:{
+        type:Number,
+        required:true,
+        unique:true
+     },
+     password:{
+        type:String,
+        required:true
+     },
+     confirmpassword:{
+        type:String,
+        required:true
+     }
+})
+
+//collection
+const Register = new mongoose.model("Register", userSchema);
+
+module.exports= Register;
